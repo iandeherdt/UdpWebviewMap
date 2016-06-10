21ad3e3a721da5f06625d3bf3c9a22fe59389ff1
iandeherdt <ian.de.herdt@telenet.be>
2016-06-10 17:13:01 +0200

    working example with focus

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
 
 chrome.app.runtime.onLaunched.addListener(function() {
-  console.log('background loaded');
   chrome.app.window.create('window.html', {
     'outerBounds': {
       'width': 1280,
-      'height': 640
+      'height': 960
     }
   });
 });
